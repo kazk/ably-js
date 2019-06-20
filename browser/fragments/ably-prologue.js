572eba9e4e88bc68cc16ed7684379fdd0b3cc7d5
Clifton Hensley <cliftonhensley@invisionapp.com>
2019-06-20 10:36:03 -0500

    Creating reference to global scope in ably-prologue in order to support the realtime client running in a worker context

--- browser/fragments/ably-prologue.js
+++ browser/fragments/ably-prologue.js
@@ -1,6 +1,12 @@
 ;(function() {
-	"use strict";
-	var Ably = window.Ably = this;
+  "use strict";
+
+  /*
+    Create a reference to the global scope - `window` for the browser window,
+    and `self` for web workers.
+  */
+	var global = (typeof window === 'object' && window) || (typeof self === 'object' && self);
+	var Ably = global.Ably = this;
 
   /*
     Prevent libraries such as msgpack plugging into AMD or CommonJS
